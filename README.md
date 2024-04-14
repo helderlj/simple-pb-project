3310fa7074dc21f992a5c9b934a8c561be26dda8
Helder Lima de Jesus <helder@Helders-iMac-Pro.local>
2024-04-13 23:42:56 -0300

    Primeiro commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# another_pb_proj
+
+A new Flutter project.
---
+++ android/app/src/main/kotlin/dev/helder/pb/another_pb_proj/MainActivity.kt
@@ -0,0 +1,5 @@
+package dev.helder.pb.another_pb_proj
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
